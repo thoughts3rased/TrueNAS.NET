5e5dd79914e0dfb98eb28468256eadc0bc9ec036
STong <STong@SAM-LAPTOP>
2024-09-14 14:20:56 +0100

    Add project files.

---
+++ TrueNAS.NET/Models/Connection/ConnectionInformation.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrueNAS.NET.Models.Connection
+{
+    public class ConnectionInformation
+    {
+        public ConnectionInformation(string apiKey, string host, int port)
+        {
+            ApiKey = apiKey;
+            Host = host;
+            Port = port;
+        }
+
+        /// <summary>
+        /// API key for authenticating with the TrueNAS API
+        /// </summary>
+        public string ApiKey { get; set; }
+
+        /// <summary>
+        /// IP or FQDN where the instance of TrueNAS is located, excluding the port
+        /// </summary>
+        public string Host {  get; set; }
+
+        /// <summary>
+        /// Port that the TrueNAS API is listening on
+        /// </summary>
+        public int Port { get; set; }
+    }
+}
---
+++ TrueNAS.NET/TrueNASApiClient.cs
@@ -0,0 +1,17 @@
+﻿using TrueNAS.NET.Models.Connection;
+
+namespace TrueNAS.NET
+{
+    /// <summary>
+    /// Represents a single TrueNAS connection, with all the methods available for the particular 
+    /// </summary>
+    public class TrueNASApiClient
+    {
+        private ConnectionInformation _connectionInformation;
+        
+        public TrueNASApiClient(string apiKey, string host, int port)
+        {
+            _connectionInformation = new(apiKey, host, port);
+        }
+    }
+}
